d6abeb03827588e954ef127b932537a1fc0fbc06
浮世 <2016143488@qq.com>
2021-11-16 01:23:03 +0800

    text2.c

---
+++ 66.c
@@ -0,0 +1,38 @@
+
+#include<math.h>
+#include <stdio.h>
+/*ÒÑÆÁ±Î*/
+//int main()
+//{
+//	printf("%d\n", sizeof(long));
+//	return 0;
+/*ÒÑÆÁ±Î*/
+//int main()
+//{
+//	int age = 18;
+//	age = age + 1;
+//	double weight = 76;
+//	weight = weight - 10;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int sum = 0;
+	sum = a * b;
+	printf("%d\n", sum);
+	return 0;
+
+
+}
